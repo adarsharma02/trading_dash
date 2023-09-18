9a7dce99fc58500647848e0f9aec091115167a1f
adarsharma02 <98862737+adarsharma02@users.noreply.github.com>
2023-09-18 10:46:31 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+function menu()
+{
+ const all_icon = document.getElementById("all-svg-icon");
+ if(all_icon.style.display=='none'){
+   all_icon.style.display = 'block';
+ }
+ else{
+   all_icon.style.display = 'none';
+ }
+ 
+}
